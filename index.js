afa791520255138e1e2e70c0f31a8b97daae835f
MasaoSasaki <daikichi852@icloud.com>
2021-08-08 00:56:46 +0900

    :hammer: Update 4 players

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // https://www.youtube.com/watch?v=K2cJofUJVO8&t=534s 45:00"
 
 getDate = () => {
-  function formatTime(tmpTime) {
+  formatTime = (tmpTime) => {
     let time = tmpTime.toString();
     if(time.length === 1) return '0' + time;
     return time;
@@ -16,46 +16,51 @@ getDate = () => {
   return `${year}/${month}/${day} ${hour}:${minute}:${second}`
 }
 
-console.log(getDate())
-// 4人の場合の組み合わせ
-let people = 4
-let match = new Array
-let MatchList = new Array
+getPattern = (num) => {
+  return Pattern = num * (num - 1) * (num - 2) * (num - 3) / 8
+}
 
-// for(i=0; i<people; i++) {
-//   team[i] = a
-// }
-let i = 0, j=0, a;
-while(MatchList.length < 24) {
-  while(match.length < people) {
+createMatch = (people) => {
+  let match = new Array
+  while(match.length < 4) {
     a = Math.floor( Math.random() * people ) + 1
-    // a = i + 1
     if (match.find(num => num === a)) continue;
     match[i] = a
     i++
   }
-  if (MatchList.find(matchL => matchL === match)) {
+  i = 0
+  return match;
+}
+
+console.log(getDate())
+
+// 入力値
+let people = 4  //　人数
+
+let MatchList = new Array
+let Pattern;
+Pattern = getPattern(people)
+
+let i = 0, j=0, a;
+while(MatchList.length < Pattern) {
+
+  // ペア作成
+  let Match = createMatch(people);
+  if (MatchList.find(MatchI => MatchI.toString() === Match.toString())) {
     i=0
-    match = []
+    Match = []
     continue;
   }
-  MatchList[j] = match
+
+  if (MatchList.find(MatchI => MatchI.slice(0,2).includes(Match[0]) && MatchI.slice(0,2).includes(Match[1]) || !MatchI.slice(0,2).includes(Match[0]) && !MatchI.slice(0,2).includes(Match[1]))) {
+    i=0
+    Match = []
+    continue;
+  }
+  MatchList[j] = Match
   j++
 }
 
 console.log(MatchList)
-
-
-// console.log(people.map(num => num + 1))
-
-
-// for(i=0; i < people.length; i++) {
-//   console.log(people)
-// }
-
-//
-// C[ C[1,2] C[3,4] ]
-//
-//git
 
 console.log(getDate())
